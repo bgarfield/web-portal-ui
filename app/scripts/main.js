4557084691f9144f7bdb2e5b11172e115e6873b5
harrymoore <harry.moore@rothburysoftware.com>
2014-02-25 15:46:21 -0500

    Restore behavior after switch to requirejs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -45,10 +45,10 @@ require([
   'models/site',
   'collections/sites',
   'templates'
-], function (Backbone, AppView, Workspace) {
+], function (Backbone, AppView, appRouter, SiteModel, SitesCollection) {
   /*jshint nonew:false*/
 
-  window.webPortalUi = {
+  var webPortalUi = {
       Models: {},
       Collections: {},
       Views: {},
@@ -66,7 +66,7 @@ require([
           // Backbone.Log.log("Collection size: " + webPortalUi.Collections.sitesPagingCollection.length);
 
           // create the main collection of sites. It will load an initial set of records automatically
-          webPortalUi.Collections.sitesCollection = new webPortalUi.Collections.SitesCollection();
+          this.Collections.sitesCollection = new SitesCollection();
           Backbone.Log.log("Collection size: " + webPortalUi.Collections.sitesCollection.length);
 
           webPortalUi.Views.pagedSiteListView = new webPortalUi.Views.SiteListPagingView({
@@ -74,7 +74,7 @@ require([
             collection: webPortalUi.Collections.sitesCollection
           });
 
-          webPortalUi.Routers.appRouter = new webPortalUi.Routers.AppRouter();
+          appRouter = new webPortalUi.Routers.AppRouter();
           Backbone.history.start();
 
           $.ajaxSetup({
@@ -93,7 +93,8 @@ require([
       }
   };
 
-  return window.webPortalUi;
+  webPortalUi.init();
+  return webPortalUi;
 });
 
 // 
