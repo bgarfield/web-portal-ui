ec3dcbfa6527aec4fe99468270bb57e29e7af9a1
Bobby Johnson <bobby.johnson@ixxus.com>
2014-02-27 13:36:57 -0500

    expanded properties of the site model

--- app/scripts/models/site.js
+++ app/scripts/models/site.js
@@ -8,7 +8,16 @@ define(['backbone'], function(Backbone) {
     defaults: {
       'siteId': '',
       'name': '',
+      'title': '',
+      'description': '',
+      'plans': [],
       'domain': '',
+      'launchDate': '',
+      'globalContentSite': false,
+      'updateRequired': true,
+      'businessPriority': 'Level A (Critical)',
+      'testingEffort': 'Small (1-3 Days)',
+      'layout': '',
       'logoUrl': 'https://financialadvisordirect.com/divinvest.ws.catalogmanagement.V_2.0/CatalogViewServlet?_x=Dyr1mr8D-4Sb2R692_ef-9cSEMABkJuw3KPhF8vMtYg.&_d=lDHb2fElVTK_1NE0RcG9ew..'
     },
 
